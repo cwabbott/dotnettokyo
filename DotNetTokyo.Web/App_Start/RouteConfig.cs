b2d0d3cb309ca59347ccf2a6a48e5aa252b94e6f
Ed Andersen <mail@edandersen.com>
2014-06-22 14:08:43 +0900

    Added background image and some text

--- DotNetTokyo.Web/App_Start/RouteConfig.cs
+++ DotNetTokyo.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace DotNetTokyo.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.LowercaseUrls = true;
         }
     }
 }
